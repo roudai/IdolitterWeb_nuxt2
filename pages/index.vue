de073f288a426fc98d9ee5157bd6edc7e2437a40
roudai <me@roudai.net>
2023-01-05 11:44:25 +0900

    baseURL変更

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,7 @@ export default {
     const baseUrl = $axios.defaults.baseURL + 'アイドル一覧?'
     const params = { key: $config.apiKey }
     const queryParams = new URLSearchParams(params)
+    console.log(baseUrl + queryParams)
     const response = await $axios.$get(baseUrl + queryParams)
     return response
   },

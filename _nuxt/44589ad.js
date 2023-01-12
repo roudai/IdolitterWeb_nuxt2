fe2c0490d2d732c93241927c39dbc5e79393374d
roudai <roudai@users.noreply.github.com>
2023-01-12 07:23:17 +0000

    deploy: 7d19ff5b0ad98d30a2468eec17cb142c4b1f9d16

---
+++ _nuxt/44589ad.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{321:function(e,n,r){"use strict";r.r(n);var o=r(304);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.15.0","app-compat")}}]);

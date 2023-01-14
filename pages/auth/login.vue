08474f0d284cd026d5252517485de87c32aaae36
roudai <me@roudai.net>
2023-01-14 11:17:20 +0900

    Twitterログイン、ログアウト実装

---
+++ pages/auth/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="button is-info mb-2" @click="socialAuth()">
+      Twitterでログインする
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { getAuth, TwitterAuthProvider, signInWithRedirect } from 'firebase/auth'
+
+export default {
+  mounted() {
+    // this.$store.dispatch('auth/login')
+  },
+  methods: {
+    socialAuth() {
+      const auth = getAuth()
+      signInWithRedirect(auth, new TwitterAuthProvider())
+    },
+  },
+}
+</script>
+
+<style></style>

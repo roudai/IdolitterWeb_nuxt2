a9aa348a2d839e7f2bdf990ab93c1af659614cfc
roudai <me@roudai.net>
2023-01-17 23:02:29 +0900

    LocalStorageにデータ保存

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h5>マイページ</h5>
+    <div>{{ user }}</div>
     <button class="button is-info mb-2" @click="socialAuth()">
       ログアウトする
     </button>
@@ -9,6 +10,14 @@
 
 <script>
 export default {
+  data() {
+    return {
+      user: '',
+    }
+  },
+  created() {
+    this.user = localStorage.getItem('user')
+  },
   methods: {
     socialAuth() {
       this.$store.dispatch('auth/logout')
--- store/auth.js
+++ store/auth.js
@@ -2,19 +2,11 @@ import { getAuth, getRedirectResult, signOut } from 'firebase/auth'
 
 export const state = () => ({
   isLoggedIn: false,
-  uid: '',
-  user: '',
 })
 
 export const mutations = {
   setLoginState(state, loggedIn) {
     state.isLoggedIn = loggedIn
-  },
-  setUid(state, uid) {
-    state.uid = uid
-  },
-  setUser(state, user) {
-    state.user = user
   },
 }
 
@@ -27,13 +19,14 @@ export const actions = {
           return
         }
         commit('setLoginState', true)
-        commit('setUid', result.user.providerData[0].uid)
-        commit('setUser', result.user.reloadUserInfo.screenName)
+        localStorage.uid = result.user.providerData[0].uid
+        localStorage.user = result.user.reloadUserInfo.screenName
         this.$router.push('/mypage')
       })
       .catch((error) => {
         const errorCode = error.code
         const errorMessage = error.message
+        // eslint-disable-next-line no-console
         console.log(errorCode, errorMessage)
       })
   },
@@ -42,23 +35,19 @@ export const actions = {
     await signOut(auth)
       .then(() => {
         commit('setLoginState', false)
-        commit('setUid', '')
-        commit('setUser', '')
+        localStorage.removeItem('uid')
+        localStorage.removeItem('user')
         this.$router.push('/auth/login')
       })
       .catch((error) => {
         alert('logout:' + error)
       })
   },
-  addUserInfo({ commit }, user) {
+  addUserInfo({ commit }) {
     commit('setLoginState', true)
-    commit('setUid', user.providerData[0].uid)
-    commit('setUser', user.reloadUserInfo.screenName)
   },
 }
 
 export const getters = {
   getLoggedIn: (state) => !!state.isLoggedIn,
-  getUid: (state) => state.uid,
-  getUser: (state) => state.user,
 }

649fb85e5a5441be1a800596a73d9f01a1abdff0
roudai <me@roudai.net>
2023-01-18 15:33:01 +0900

    ログインページへのリンク

--- pages/auth/redirect.vue
+++ pages/auth/redirect.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>Twitterにログインしています...</div>
+  <div>
+    <h5>Twitterにログインしています...</h5>
+    <nuxt-link v-show="show" to="/auth/login">ログインページに戻る</nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
   created() {
     this.$store.dispatch('auth/login')
+    setTimeout(() => {
+      this.show = true
+    }, 5000)
   },
 }
 </script>

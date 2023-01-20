aaeddceae7d1146c530928d13eb7a67ba266c414
roudai <me@roudai.net>
2023-01-20 18:19:47 +0900

    フォーム作成

--- pages/mypage/add/_uid.vue
+++ pages/mypage/add/_uid.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
-    チェキレポート追加
+    <div v-show="show" class="mb-3">
+      <h3>{{ name }}</h3>
+      <h4>{{ group }}</h4>
+      <b-field label="日付">
+        <b-datepicker
+          v-model="selected"
+          locale="ja-JP"
+          placeholder="日付を選択してください"
+          icon="calendar-today"
+          :icon-right="selected ? 'close-circle' : ''"
+          icon-right-clickable
+          trap-focus
+          @icon-right-click="clearDate"
+        >
+        </b-datepicker>
+      </b-field>
 
-    {{ $route.params.uid }}
+      <b-field label="枚数">
+        <b-numberinput
+          v-model="number"
+          :editable="false"
+          type="is-light"
+          min="1"
+          max="100"
+        ></b-numberinput>
+      </b-field>
+
+      <b-field label="URL">
+        <b-input v-model="url" placeholder="https://"></b-input>
+      </b-field>
+    </div>
+
+    <div class="buttons">
+      <b-button type="is-link is-light">登録</b-button>
+      <b-button type="is-link is-light" @click="$router.go(-1)">戻る</b-button>
+    </div>
   </div>
 </template>
 
@@ -11,7 +44,14 @@ import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 export default {
   data() {
-    return {}
+    return {
+      show: true,
+      group: '',
+      name: '',
+      url: '',
+      selected: new Date(),
+      number: 1,
+    }
   },
   created() {
     setTimeout(async () => {
@@ -24,11 +64,18 @@ export default {
       const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        console.log(docSnap.data())
+        this.group = docSnap.data().group
+        this.name = docSnap.data().name
       } else {
-        console.log('エラー')
+        this.show = false
       }
+      console.log(this.show)
     }, 0)
+  },
+  methods: {
+    clearDate() {
+      this.selected = null
+    },
   },
 }
 </script>

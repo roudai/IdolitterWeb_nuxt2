2a76e46d32dc33f30590c552f4e33a15fb01a36a
roudai <me@roudai.net>
2023-02-02 12:43:14 +0900

    authDomain変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,7 +102,7 @@ export default {
   firebase: {
     config: {
       apiKey: 'AIzaSyAKYSs2c7mrtmESfvcE0TQAVCAy3YtKaV8',
-      authDomain: 'idolitter-373101.firebaseapp.com',
+      authDomain: 'idolitter.net',
       projectId: 'idolitter-373101',
       storageBucket: 'idolitter-373101.appspot.com',
       messagingSenderId: '553829081488',

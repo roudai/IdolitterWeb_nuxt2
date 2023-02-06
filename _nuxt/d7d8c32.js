1582bdac9899b031fb55ed9b2597149e75e61873
roudai <roudai@users.noreply.github.com>
2023-02-06 14:33:42 +0000

    deploy: cde5917c5cccb6c1b23e658e5cfb46367f938f78

---
+++ _nuxt/d7d8c32.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{368:function(n,t,l){"use strict";l.r(t);var o={},e=l(37),component=Object(e.a)(o,(function(){var n=this._self._c;return n("div",[n("dash-board")],1)}),[],!1,null,null,null);t.default=component.exports;installComponents(component,{DashBoard:l(342).default})}}]);

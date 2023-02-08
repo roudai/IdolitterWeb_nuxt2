dd058925edab573ca5e8894c1ace19604f18a2c1
roudai <roudai@users.noreply.github.com>
2023-02-08 12:52:10 +0000

    deploy: 799b5af0e807d39e66ad731e2aca140ea770999d

--- _nuxt/static/1675859216/auth/logout/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675859216",layout:"default",error:b,state:{auth:{isLoggedIn:false,uid:a,user:a,displayName:a}},serverRendered:true,routePath:"\u002Fauth\u002Flogout",config:{apiKey:"AIzaSyBiIUR359_DPStoJKJUtyv6Vzyfw4BU2oY",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b}}}}("",null));
